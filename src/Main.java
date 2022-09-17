fb974cd3f002b477f59d6aba02643e83fdbffb6e
Guy Marshall <guymarshall9@gmail.com>
2022-09-17 22:07:10 +0100

    Change back to IntelliJ

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
-package com.marshall.guy;
-
 import java.util.Scanner;
 
-public class App 
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a positive integer: ");
         int iterations = scanner.nextInt();
         scanner.close();
 
-        for (double i = 1.0; i <= iterations; i++)
+        for (int i = 1; i <= iterations; i++)
         {
-            System.out.printf("%f: %f%n", i, Math.pow((1 + 1.0/i), i));
+            System.out.printf("%d: %f%n", i, Math.pow((1 + 1.0/i), i));
         }
     }
 }
